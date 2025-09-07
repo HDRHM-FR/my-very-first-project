199cbda75a89d913d74f142a7a8ab5d4ec5232ef
hadi <hadi.network.go@gmail.com>
2025-09-07 23:52:47 +0330

    calledAndCommentedAllPackages

--- main.go
+++ main.go
@@ -1,16 +1,18 @@
 package main
 
 import (
-	//"github.com/my-very-first-project/Bank"
-	Quadraticequation "github.com/my-very-first-project/Quadratic-Equation"
-	//"github.com/my-very-first-project/Bank"
+//"github.com/my-very-first-project/Interface"
+//Quadraticequation "github.com/my-very-first-project/Quadratic-Equation"
+//"github.com/my-very-first-project/Bank"
+//"github.com/my-very-first-project/Arrays"
+
 )
 
 func main() {
 
 	//Interface.Salam()
 	//Arrays.Array()
-	Quadraticequation.Math()
+	//Quadraticequation.Math()
 	//Bank.Welcome()
 
 }
