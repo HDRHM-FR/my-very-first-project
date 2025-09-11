6b6d4ff63466f036b1ca0d33f10e7bdbcb077d41
hadi <hadi.network.go@gmail.com>
2025-09-11 09:54:07 +0330

    changedSomeCodes

--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	//"https/github.com/HDRHM-FR/my-very-first-project.git/Interface"
-	//Quadraticequation "https/github.com/HDRHM-FR/my-very-first-project.git/Quadratic-Equation"
-	//"https/github.com/HDRHM-FR/my-very-first-project.git/Bank"
-	//"https/github.com/HDRHM-FR/my-very-first-project.git/Arrays"
-	"https/github.com/HDRHM-FR/my-very-first-project.git/Error"
+// "https/github.com/HDRHM-FR/my-very-first-project.git/Interface"
+// Quadraticequation "https/github.com/HDRHM-FR/my-very-first-project.git/Quadratic-Equation"
+// "https/github.com/HDRHM-FR/my-very-first-project.git/Bank"
+// "https/github.com/HDRHM-FR/my-very-first-project.git/Arrays"
 )
 
 func main() {
@@ -14,6 +13,5 @@ func main() {
 	//Arrays.Array()
 	//Quadraticequation.Math()
 	//Bank.Welcome()
-	Error.Err()
 
 }
