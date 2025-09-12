3152ad618012d7d3483915d355af01429ed453f2
hadi <hadi.network.go@gmail.com>
2025-09-12 10:34:20 +0330

    added a simple sum with errors

--- Error/err.go
+++ Error/err.go
@@ -6,27 +6,29 @@ import (
 
 func ER() {
 	var number1, number2 int
+	fmt.Printf("please enter the first number :\n")
 	for {
-		fmt.Printf("please enter the first number :\n")
 		_, err1 := fmt.Scanln(&number1)
 		if err1 != nil {
 			fmt.Printf("error found !!!\n")
+			fmt.Println("please try again : ")
 			continue
 		} else {
 			break
 		}
 	}
-
+	fmt.Println("enter second number : ")
 	for {
-		fmt.Println("enter second number : ")
 		_, err2 := fmt.Scanln(&number2)
 		if err2 != nil {
 			fmt.Println("error found !!!")
+			fmt.Println("please try again : ")
 			continue
 		} else {
 			fmt.Printf("result : %v", number1+number2)
 			break
 		}
+
 	}
 
 }
