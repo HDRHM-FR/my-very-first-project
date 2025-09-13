0e62e7ef7fa96b416cc5770a8b40e10a0453b561
hadi <hadi.network.go@gmail.com>
2025-09-13 14:11:12 +0330

    addedErrorsPackageAndDescribedIt

--- Error/err.go
+++ Error/err.go
@@ -1,34 +1,56 @@
 package Error
 
 import (
+	"errors"
 	"fmt"
 )
 
+// section : Error
 func ER() {
-	var number1, number2 int
-	fmt.Printf("please enter the first number :\n")
-	for {
-		_, err1 := fmt.Scanln(&number1)
-		if err1 != nil {
-			fmt.Printf("error found !!!\n")
-			fmt.Println("please try again : ")
-			continue
-		} else {
-			break
-		}
-	}
-	fmt.Println("enter second number : ")
-	for {
-		_, err2 := fmt.Scanln(&number2)
-		if err2 != nil {
-			fmt.Println("error found !!!")
-			fmt.Println("please try again : ")
-			continue
-		} else {
-			fmt.Printf("result : %v", number1+number2)
-			break
-		}
+	// var (
+	// 	number1 int
+	// 	number2 int
+	// )
+	// fmt.Printf("please enter the first number :\n")
+	// for {
+	// 	_, err1 := fmt.Scanln(&number1)
+	// 	if err1 != nil {
+	// 		fmt.Println("error found !!!\nplease enter a number : ")
+	// 		continue
+	// 	} else {
+	// 		break
+	// 	}
+	// }
+	// fmt.Println("enter second number : ")
+	// for {
+	// 	_, err2 := fmt.Scanln(&number2)
+	// 	if err2 != nil {
+	// 		fmt.Printf("\nerror found !!!\nplease enter a number : ")
+	// 		continue
+	// 	} else {
+	// 		break
+	// 	}
 
-	}
+	// }
 
+	// fmt.Printf("result : %v", number1+number2)
+
+	// section : errors package
+	// note : this method is more efficiant
+
+	_, err3 := sum()
+	if err3 != nil {
+		panic(err3.Error())
+
+	} else {
+		fmt.Print("result : ")
+		fmt.Println(sum())
+	}
+}
+
+func sum() (int, error) {
+	var a, b int
+	fmt.Scanln(&a)
+	fmt.Scanln(&b)
+	return a + b, errors.New("sum couldnt perform, please enter a valid number")
 }
