9e38aa48da6f3f2dc4fb273dc35e264e34640d04
hadi <hadi.network.go@gmail.com>
2025-09-17 22:52:20 +0330

    commited the modified package goroutine

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
-import "https/github.com/HDRHM-FR/my-very-first-project.git/Goroutine"
-
-// "https/github.com/HDRHM-FR/my-very-first-project.git/Error"
-//"https/github.com/HDRHM-FR/my-very-first-project.git/Interface"
-//Quadraticequation "https/github.com/HDRHM-FR/my-very-first-project.git/Quadratic-Equation"
-//"https/github.com/HDRHM-FR/my-very-first-project.git/Bank"
-//"https/github.com/HDRHM-FR/my-very-first-project.git/Arrays"
+import (
+	"https/github.com/HDRHM-FR/my-very-first-project.git/Goroutine"
+	// "https/github.com/HDRHM-FR/my-very-first-project.git/Error"
+	//"https/github.com/HDRHM-FR/my-very-first-project.git/Interface"
+	//Quadraticequation "https/github.com/HDRHM-FR/my-very-first-project.git/Quadratic-Equation"
+	//"https/github.com/HDRHM-FR/my-very-first-project.git/Bank"
+	//"https/github.com/HDRHM-FR/my-very-first-project.git/Arrays"
+)
 
 func main() {
 
