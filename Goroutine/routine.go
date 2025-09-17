ab50c5e3231e28d3ffe00bcffbc06b9b12895446
hadi <hadi.network.go@gmail.com>
2025-09-17 22:51:54 +0330

    added error handling in goroutine package

--- Goroutine/routine.go
+++ Goroutine/routine.go
@@ -7,20 +7,40 @@ import (
 
 // section : go routine
 // note : it is a simple mathmatic written with go routine
+// HEADS UP : i added the error handling to underestand it more
 
 func Routine() {
-	var (
-		a = 15
-		b = 9
-		c = a + b
-		d = 8 - 3
-	)
 	go func() {
 		time.Sleep(29 * time.Millisecond)
-		fmt.Printf("first value :%v\n", c)
-		fmt.Printf("second value :%v\n", d)
+		_, err := Err(s, 4, 6, 2)
+		if err != nil {
+			panic("")
+		} else {
+			return
+		}
 	}()
-
 	time.Sleep(30 * time.Millisecond)
-	fmt.Println(c * d)
+}
+
+func Err(a, b, c, d float64) (float64, error) {
+	var (
+		e = a + b
+		f = c + d
+	)
+	for {
+		_, err1 := fmt.Printf("first value :%v\n", e)
+		if err1 != nil {
+			fmt.Printf("please enter a valid number :%v\n ", err1)
+			continue
+		}
+		_, err2 := fmt.Printf("second value :%v\n", f)
+		if err2 != nil {
+			fmt.Printf("please enter a valid number :%v\n ", err2)
+			break
+		} else {
+			fmt.Printf("result :%v\n", f*e)
+		}
+	}
+	return 0, nil
+
 }
