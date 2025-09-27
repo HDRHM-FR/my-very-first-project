7f0aa840edb8412354891db3fc65b9e23b60f16b
hadi <hadi.network.go@gmail.com>
2025-09-27 09:55:36 +0330

    deleted the sqlite imported packages in main package

--- main.go
+++ main.go
@@ -1,21 +1,5 @@
 package main
 
-import (
-	"https/github.com/HDRHM-FR/my-very-first-project.git/Goroutine"
-	// "https/github.com/HDRHM-FR/my-very-first-project.git/Error"
-	//"https/github.com/HDRHM-FR/my-very-first-project.git/Interface"
-	//Quadraticequation "https/github.com/HDRHM-FR/my-very-first-project.git/Quadratic-Equation"
-	//"https/github.com/HDRHM-FR/my-very-first-project.git/Bank"
-	//"https/github.com/HDRHM-FR/my-very-first-project.git/Arrays"
-)
-
 func main() {
-
-	//Interface.Salam()
-	//Arrays.Array()
-	//Quadraticequation.Math()
-	//Bank.Welcome()
-	//Error.ER()
-	Goroutine.Routine()
 
 }
