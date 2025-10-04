09893a4d6abcbf9bba6e9613a885200579034a14
hadi <hadi.network.go@gmail.com>
2025-10-04 17:42:42 +0330

    added bank database for testing

---
+++ database/bank.sql
@@ -0,0 +1,19 @@
+--create TABLE atm(
+--    atmfullname TEXT NOT NULL PRIMARY KEY,
+--    atmpass INTEGER NOT NULL,
+--    atmusername TEXT NOT NULL,
+--    atmemail TEXT NOT NULL
+--);
+--INSERT INTO
+--    atm(atmfullname, atmpass, atmusername, atmemail)
+--VALUES
+--    (
+--        "hadi rahimi",
+--        13801380,
+--        "HDRHM",
+--        "hadi.rahimi@gmail.com"
+--    )
+SELECT
+    *
+FROM
+    atm;
